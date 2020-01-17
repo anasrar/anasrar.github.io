cc2f53bd9c59d8aac14c4a23def1cb0c6cf58853
Anas RAR <mynameanasrar@gmail.com>
2020-01-17 18:05:21 +0700

    compress bg to webp and add music player

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-// custom Js
+document.addEventListener("DOMContentLoaded", function () {
+    let player = document.querySelector("#player"),
+        audio = player.querySelector("#audio"),
+        control = player.querySelector("#control"),
+        playBtn = control.querySelector("#playBtn>svg>path");
+
+    playBtn.addEventListener("click", function () {
+        if (audio.paused) {
+            audio.play();
+            playBtn.setAttribute("d", "M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26")
+        } else {
+            audio.pause();
+            playBtn.setAttribute("d", "M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28")
+        }
+    })
+})
