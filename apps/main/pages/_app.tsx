7bcbac7089e1c8776bf0abbcec9412d5c9363c7f
anasrar <mynameanasrar@gmail.com>
2023-01-05 14:36:37 +1300

    feat: using `@next/font` for default font

--- apps/main/pages/_app.tsx
+++ apps/main/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
 import {
 	ColorScheme,
 	ColorSchemeProvider,
@@ -11,6 +12,52 @@ import "../styles/app.scss";
 import { Loadbar } from "components/global/Loadbar";
 import { Header } from "components/global/Header";
 import { Footer } from "components/global/Footer";
+
+const openSauceTwoFonts = localFont({
+	variable: "--font-open-sauce-two",
+	src: [
+		{
+			path: "../fonts/OpenSauceTwo/OpenSauceTwo-Regular.ttf",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../fonts/OpenSauceTwo/OpenSauceTwo-Italic.ttf",
+			weight: "400",
+			style: "italic",
+		},
+		{
+			path: "../fonts/OpenSauceTwo/OpenSauceTwo-Medium.ttf",
+			weight: "500",
+			style: "normal",
+		},
+		{
+			path: "../fonts/OpenSauceTwo/OpenSauceTwo-MediumItalic.ttf",
+			weight: "500",
+			style: "italic",
+		},
+		{
+			path: "../fonts/OpenSauceTwo/OpenSauceTwo-SemiBold.ttf",
+			weight: "600",
+			style: "normal",
+		},
+		{
+			path: "../fonts/OpenSauceTwo/OpenSauceTwo-SemiBoldItalic.ttf",
+			weight: "600",
+			style: "italic",
+		},
+		{
+			path: "../fonts/OpenSauceTwo/OpenSauceTwo-Bold.ttf",
+			weight: "700",
+			style: "normal",
+		},
+		{
+			path: "../fonts/OpenSauceTwo/OpenSauceTwo-BoldItalic.ttf",
+			weight: "700",
+			style: "italic",
+		},
+	],
+});
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
 	const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
@@ -30,7 +77,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 			<MantineProvider
 				theme={{
 					colorScheme,
-					fontFamily: `Open Sauce Two, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Source Han Sans, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji`,
+					fontFamily: `${openSauceTwoFonts.style.fontFamily}, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Source Han Sans, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji`,
 					fontFamilyMonospace: `JetBrains Mono VF, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace`,
 				}}
 				withGlobalStyles
