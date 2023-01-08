eed221fdfcf40cea17e0009fc71f24c59b025004
anasrar <mynameanasrar@gmail.com>
2023-01-08 23:35:43 +1300

    fix: `jsonld` hydration

--- apps/main/components/global/LinkedDataJSON/component.tsx
+++ apps/main/components/global/LinkedDataJSON/component.tsx
@@ -1,11 +1,16 @@
+import Head from "next/head";
 import { FC } from "react";
 import { LinkedDataJSONProps } from "./props";
 
 export const LinkedDataJSON: FC<LinkedDataJSONProps> = ({ data }) => {
 	return (
-		<script
-			type="application/ld+json"
-			dangerouslySetInnerHTML={{ __html: JSON.stringify(data, null, 2) }}
-		/>
+		<Head>
+			<script
+				type="application/ld+json"
+				dangerouslySetInnerHTML={{
+					__html: JSON.stringify(data, null, 2),
+				}}
+			/>
+		</Head>
 	);
 };
