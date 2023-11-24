983ae0238fa586e5860bb22531e84203a57d516d
anasrar <mynameanasrar@gmail.com>
2023-11-24 13:57:41 +1300

    feat(blog, og-image): update `Inter` v4

---
+++ packages/blog/src/fonts/Inter/generate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+fonttools varLib.mutator -o Inter-400.ttf ./InterV.var.ttf wght=400 opsz=22
+fonttools varLib.mutator -o Inter-500.ttf ./InterV.var.ttf wght=500 opsz=22
+fonttools varLib.mutator -o Inter-600.ttf ./InterV.var.ttf wght=600 opsz=22
+fonttools varLib.mutator -o Inter-700.ttf ./InterV.var.ttf wght=700 opsz=22
+
+fonttools varLib.mutator -o Inter-Italic-400.ttf ./InterV-Italic.var.ttf wght=400 opsz=22
+fonttools varLib.mutator -o Inter-Italic-500.ttf ./InterV-Italic.var.ttf wght=500 opsz=22
+fonttools varLib.mutator -o Inter-Italic-600.ttf ./InterV-Italic.var.ttf wght=600 opsz=22
+fonttools varLib.mutator -o Inter-Italic-700.ttf ./InterV-Italic.var.ttf wght=700 opsz=22
